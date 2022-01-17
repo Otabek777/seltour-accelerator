84012aed89f2ab57b18041fb76b9047d4d9f844f
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2022-01-17 14:32:56 +0500

    поправки

--- js/main.js
+++ js/main.js
@@ -20,3 +20,18 @@ document.querySelector('.header__burgir').addEventListener('click', function() {
     document.querySelector('.header_menu').classList.toggle('active');
     document.querySelector('body').classList.toggle('hidden')
 });
+
+const optionBtns = document.querySelectorAll('.option .btn');
+for(let i = 0; i < optionBtns.length; i++) {
+    optionBtns[i].addEventListener('click', function() {
+        let optionBtnsThis = this;
+        setTimeout(function() {
+            optionBtnsThis.classList.add('active');
+        }, 01);
+    });
+};
+document.querySelector('body').addEventListener('click', function() {
+    for(let i = 0; i < optionBtns.length; i++) {
+        optionBtns[i].classList.remove('active')
+    };
+});
